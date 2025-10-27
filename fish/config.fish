80ee85c53deab6ad9568d0594296b6f934e7a1d9
Yuning Suen <yuningsuen@gmail.com>
2025-10-27 10:16:33 +0800

    fix: tackle bugs and polish UI

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,10 @@ if status is-interactive
 end
 
 starship init fish | source
- 
-alias hg="history | grep"
+
+alias hg='history | grep'
+alias wff='wf-recorder -f ~/Videos/recording_(date +%Y-%m-%d_%H-%M-%S).mp4'
+alias wf='wf-recorder -g "$(slurp)" -f ~/Videos/recording_(date +%Y-%m-%d_%H-%M-%S).mp4'
 
 takeoff
 fastfetch
