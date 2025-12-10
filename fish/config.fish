a4193c274f588b496ab36619418b0f45dbd56bcf
Yuning Suen <yuningsuen@gmail.com>
2025-12-10 13:19:02 +0800

    fix: tidy code up, remove obsolete lines

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,5 @@ end
 starship init fish | source
 
 alias hg='history | grep'
-alias wff='wf-recorder -f ~/Videos/recording_(date +%Y-%m-%d_%H-%M-%S).mp4'
-alias wf='wf-recorder -g "$(slurp)" -f ~/Videos/recording_(date +%Y-%m-%d_%H-%M-%S).mp4'
 
-takeoff
 fastfetch
