9aaf097059d7b349f87399cb00d1db6917601528
yuningsuen <yuningsuen@gmail.com>
2026-05-11 09:59:52 +0800

    fix: adapt for macos

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
+/opt/homebrew/bin/brew shellenv | source
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
 end
-
-starship init fish | source
 
 alias hg='history | grep'
 
